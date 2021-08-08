a47b0a72fcff9714a4e9eed29ced0161ed141e29
Eduardo Trejo <rotjeot@gmail.com>
2021-08-08 18:04:49 -0500

    modificacion de algunas interfaces

--- py/minkowsk.py
+++ py/minkowsk.py
@@ -2,7 +2,7 @@ from scipy.spatial import distance
 import pandas as pd
 
 
-def euclidean(path):
+def minkowski(path):
     data = pd.read_csv(path)
     print(data)
     Lista = []
@@ -10,14 +10,13 @@ def euclidean(path):
         Lista.append([int(data.values[i,j]) 
         for j in range(1, 9)])
 
-    distancias_euclideana=[]
+    distancias_minkowski=[]
 
     for i in range(0,14):
         a = []
         for j in range(0,14):
-            a.append(distance.euclidean(Lista[i], Lista[j]))
-        distancias_euclideana.append(a)
-    df_euclideano = pd.DataFrame(distancias_euclideana)
+            a.append(distance.minkowski(Lista[i], Lista[j]))
+        distancias_minkowski.append(a)
+    df_minkowski = pd.DataFrame(distancias_minkowski)
     
-    return df_euclideano
-
+    return df_minkowski
